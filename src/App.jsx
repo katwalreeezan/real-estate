e57f6f2c596b318cfe427652808811ec0272018e
unknown <chemankatwal111@gmail.com>
2023-06-27 21:50:29 +0545

    Homepage Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import Home from './components/Home'
 
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Realestate from './components/Realestate'
+import Construction from './components/construction'
+import Consultancy from './components/Consultancy'
+import Contact from './components/Contact'
 
 function App() {
 
 
   return (
-    <>
-   <Home/>
-    </>
+    <div>
+    <BrowserRouter>
+    <Routes>
+     <Route path='/' element={<Home/>}/>
+     <Route path='/estate' element={<Realestate/>}/>
+     <Route path='/construction' element={<Construction/>}/>
+     <Route path='/consultancy' element={<Consultancy/>}/>
+     <Route path='/contact' element={<Contact/>}/>
+     
+     
+    </Routes>
+    </BrowserRouter>
+   </div>
+  
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import { MagnifyingGlass } from "phosphor-react";
 
 const Home = () => {
   return (
     <div>
-      Real Estate
-    </div>
-  )
-}
+      <div className="real-estate position-relative">
+        <div className="d-flex justify-content-between align-items-center text-white menu ">
+          <div className="d-flex align-items-center ">
+          <img src="./images/logo.png" className="d-block logo mx-4"/>
+          </div>
+          <div className="d-flex align-items-center px-5 justify-content-between">
+            <Link
+              to="/"
+              className="text-white  d-block text-decoration-none me-4"
+            >
+              Home
+            </Link>
+            <Link
+              to="/estate"
+              className="text-white d-block text-decoration-none me-4"
+            >
+              Real Estate
+            </Link>
+            <Link
+              to="/construction"
+              className="text-white d-block text-decoration-none me-4"
+            >
+              Construction
+            </Link>
+            <Link
+              to="/consultancy"
+              className="text-white d-block text-decoration-none me-4"
+            >
+              Consultancy
+            </Link>
+            <Link
+              to="/contact"
+              className="text-white d-block text-decoration-none me-4"
+            >
+              Contact Us
+            </Link>
 
-export default Home
+            <button className="btn btn-outline-light rounded-5 px-4">
+              Get Started
+            </button>
+          </div>
+        </div>
+
+        <div className="absolute">
+          <p className="text-white title col-8">Find Your Place of Dream</p>
+          <p className="text-white fw-light paragraph">
+            We are glad to have you around. Feel
+            <br /> free to browse our website
+          </p>
+          <div className="bg-light rounded-4 col-lg-7 d-flex justify-content-between align-items-center py-2 px-4">
+            <input
+              type="text"
+              placeholder="Find a location"
+              class="  border-0 outline-0 d-block"
+            />
+            <MagnifyingGlass size={22} />
+          </div>
+        </div>
+        <div className="bottom ">
+          <span className="bottomtitle ">Let Us match you with</span>
+          <br />
+          <span className="bottomtitle color">your perfect rental</span><br/>
+         <button className='btn btn-outline-primary text-dark rounded-5 mt-2'>Learn More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
