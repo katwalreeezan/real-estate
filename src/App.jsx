940ff5c8eb3289e494498f65c0727004637ab3fc
thatguynischal <justchalchal@gmail.com>
2023-07-15 20:09:24 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import Home from './components/Home'
+import Home from "./components/Home";
 
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Realestate from './components/Realestate'
-import Consultancy from './components/Consultancy'
-import Contact from './components/Contact'
-import Construction from './components/construction'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Realestate from "./components/Realestate";
+import Consultancy from "./components/Consultancy";
+import Contact from "./components/Contact";
+import Construction from "./components/Construction";
 
 function App() {
-
-
   return (
     <div>
-    <BrowserRouter>
-    <Routes>
-     <Route path='/' element={<Home/>}/>
-     <Route path='/estate' element={<Realestate/>}/>
-     <Route path='/construction' element={<Construction/>}/>
-     <Route path='/consultancy' element={<Consultancy/>}/>
-     <Route path='/contact' element={<Contact/>}/>
-     
-     
-    </Routes>
-    </BrowserRouter>
-   </div>
-  
-  )
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/estate" element={<Realestate />} />
+          <Route path="/construction" element={<Construction />} />
+          <Route path="/consultancy" element={<Consultancy />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
